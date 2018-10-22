9a89a436f3601e545e14499a5363d1c5babf8ad8
NatashaSenah <tashsenah@gmail.com>
2018-10-22 09:08:38 +0300

    add the new model

--- jirani/models.py
+++ jirani/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Neighbourhood(models.Model):
+    neighbourhood_name = models.TextField()
+    neighbourhood_location = models.TextField()
+    neighbourhood_occupant = models.TextField()
+    def __str__(self):
+        return self.image_name
+    def create_neighbourhood(self):
+        self.create()
+    def delete_neighbourhood(self):
+        self.delete()
+    
