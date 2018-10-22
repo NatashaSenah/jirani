232a25aef31631926671ee091ecc7e134b1601a0
NatashaSenah <tashsenah@gmail.com>
2018-10-22 12:37:07 +0300

    add the modified jirani

--- jirani/views.py
+++ jirani/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render,redirect
 from django.http  import HttpResponse
+from django.contrib.auth.decorators import login_required.
 
 # Create your views here.
+@login_required(login_url='/accounts/login/')
 def welcome(request):
     return render(request, 'jirani.html')
+@login_required(login_url='/accounts/login/')
 def jirani(request):
     return render(request,'jirani.html')
 def search_results(request):
